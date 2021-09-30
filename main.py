3cf796ea99239121a65a8fb88fd70a8c9621a84e
Siim Laineste <siim.laineste@outlook.com>
2021-09-30 09:45:16 +0300

    removed unneeded libraries

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from pymongo import ALL,MongoClient
 import ast
-from bson.json_util import dumps
-from bson.json_util import loads
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,12 +14,22 @@ class Name(Resource):
         collection = db['attractions']
         # had to make id not show, because it threw a not json serializable error.
         retrieved = list(collection.find({}, {'_id' : False}))
-        return loads(dumps(retrieved)), 200
+        return retrieved, 200
 
     def post(self):
-        pass
+        return 401
+    
+    def put(self):
+        return 401
+
+    def delete(self):
+        return 401
+
+    def push(self):
+        return 401
 
 api.add_resource(Name, '/name')
+
 
 if __name__ == '__main__':
     app.run()
